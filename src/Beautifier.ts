5abee94c0e5ff92e32803c2744a45bef76d9e2b3
mmelluso <mmelluso@topcon.com>
2018-03-30 11:18:37 +0200

    Fix beautify a selection

--- src/Beautifier.ts
+++ src/Beautifier.ts
@@ -1,29 +1,33 @@
-import * as vscode from 'vscode';
-import * as jsbeautify from 'js-beautify';
+import * as jsbeautify from "js-beautify";
+import * as vscode from "vscode";
+
 const tabSize = vscode.workspace.getConfiguration('beautify').get<number>("tabSize");
 const options = vscode.workspace.getConfiguration('beautify').get<number>("options");
 
 export default class Beautifier {
     public static beautify(document: vscode.TextDocument, range?: vscode.Range): any {
-        let activeEditor = vscode.window.activeTextEditor;
+        let editor = vscode.window.activeTextEditor;
 
-        if (!activeEditor) {
+        if (!editor) {
             return;
         }
 
-        if (range === null) {
-            var start = new vscode.Position(0, 0);
-            var end = new vscode.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
-            range = new vscode.Range(start, end);
+        let start = new vscode.Position(0, 0);
+        let end = new vscode.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
+
+        if (!editor.selection.isEmpty) {
+            start = editor.selection.start;
+            end = editor.selection.end;
         }
+
+        range = new vscode.Range(start, end);
         let originalText: string = document.getText(range);
         let formattedText: string = jsbeautify.css(originalText, Object.assign({
             indent_size: tabSize
         }, options));
+
         if (formattedText && originalText !== formattedText) {
-            activeEditor.edit(function (editor) {
-                var start = new vscode.Position(0, 0);
-                var end = new vscode.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
+            editor.edit(function (editor) {
                 range = new vscode.Range(start, end);
                 editor.replace(range, formattedText);
             });
